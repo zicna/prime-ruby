23ef0071ae02cf9d3a4eea109b561d761c7fa7e8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-17 16:21:16 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,27 @@
 # Add  code here!
 
+# def prime?(number)
+#     if number <= 1
+#         return false
+#     else
+#         range = (2..(number -1)).to_a
+#         #puts range
+#         if range.any? {|i| number % i == 0}
+#             return false
+#         end
+#         return true
+#     end
+# end
+#puts prime?(5)
+
 def prime?(number)
-    if number <= 1
+    if number < 2
         return false
     else
-        range = (2..(number -1)).to_a
-        #puts range
-        if range.any? {|i| number % i == 0}
+        if (2...number).any? {|num| number % num == 0}
             return false
+        else 
+            return true
         end
-        return true
     end
 end
-
-
-#puts prime?(5)
